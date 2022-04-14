a94aecaa8f5b174bb9b866c6cd9d582679bfda0a
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-04-14 21:27:00 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@
 
 function Calculate_volume() {
   // input
-  let Side_a = parseInt( (document.getElementById('Side-a-here').value));
-  let Side_b = parseInt( (document.getElementById('Side-b-here').value));
-  let Height = parseInt((document.getElementById('Height-here').value));
+  let Radius = parseInt( (document.getElementById('Radius-here').value));
+
   
   // naming constants and stuff
-  const Half_rate = 0.5;
+  const Sphere_rate = 1.333333333;
   
-  let Sphere_volume = (Side_a + Side_b) * 0.5 * Height;
+  let Sphere_volume = Radius * Radius * Radius * Math.PI * Sphere_rate;
   // remember to use .toFixed(2) to round to two decimal places
   
   // output for area
@@ -19,7 +18,8 @@ function Calculate_volume() {
 
   // unused/currently testing code:
 
-  
+  //   let Side_b = parseInt( (document.getElementById('Side-b-here').value));
+  //   let Height = parseInt((document.getElementById('Height-here').value));  
 
   
 }
